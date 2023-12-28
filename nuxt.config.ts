710daec07ecaf85844c54e6fa0e61e93732874a9
rd.zhaoxin <rd.zhaoxin@foxmail.com>
2023-12-28 16:54:01 +0800

    global style

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  css: ['~/assets/css/global.css'],
+  devtools: {
+    enabled: true,
+  },
+  css: [
+    '~/assets/css/global.css',
+  ],
   modules: [
     '@unocss/nuxt',
   ],
